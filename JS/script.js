99c93aeeee9eb58c6e72c4057a27e5c1677dcfd9
pjkbsn <philip.jkbsn@gmail.com>
2023-10-29 20:35:23 +0100

    försökTillOverlay

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,56 @@
+//Hämta data
+//
+
 // Ange URL:en för JSON-data
 const BASE_URL = 'https://majazocom.github.io/Data/solaris.json';
-
+let planetData;
+const allPlanets = document.querySelectorAll('.allElements')
 // Gör en API-förfrågan med fetch
-fetch(BASE_URL)
-    .then(response => {
+const fetchData = async () => {
+    try {
+        const response = await fetch(BASE_URL);
         if (!response.ok) {
-            throw new Error('Error fetching data');
+            throw new Error("Failed to load data");
         }
-        return response.json(); // Parsa JSON-svaret
+        const data = await response.json(); //omvandlar json till javascript object
+        planetData = data;
+        console.log(data);
+
+    }
+    catch (error) {
+        console.error("Error: ", error);
+    }
+};
+
+const planetInfo = (planetId) => {
+    const planet = planetData.find(item => item.name === planetId);
+    if (planet) {
+        // Här kan du visa informationen om planeten, t.ex. i en modal eller någon annan del av din webbsida
+        // Koppla overlay av/på här?
+        document.getElementById("overlayContainer").style.display = "block";
+
+        console.log(planet);
+    } else {
+        console.log("Planet not found in data");
+    }
+};
+
+allPlanets.forEach(function (planet) {
+    planet.addEventListener("click", function () {
+        const clickedElementId = planet.id;
+        planetInfo(clickedElementId);
+        /* console.log(clickedElementId); */
     })
-    .then(data => {
-        // Använd data från JSON
-        console.log(data); // Logga data till konsolen
+})
+/* 
+function displayOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function displayOff() {
+    document.addEventListener("click", function () {
+        document.getElementById("overlay").style.display = "none";
     })
-    .catch(error => {
-        console.error('Fel:', error); // Logga eventuella fel till konsolen
-    });
+}
+ */
+fetchData(); 

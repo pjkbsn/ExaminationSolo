9d938bff0d38be010497c7a1f37d73c2820565b7
pjkbsn <philip.jkbsn@gmail.com>
2023-11-01 12:05:48 +0100

    nästanKlar

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@
 // Ange URL:en för JSON-data
 const BASE_URL = 'https://majazocom.github.io/Data/solaris.json';
 let planetData;
-const allPlanets = document.querySelectorAll('.allElements')
+const allPlanets = document.querySelectorAll('.ourGalaxy')
 // Gör en API-förfrågan med fetch
 const fetchData = async () => {
     try {
@@ -22,12 +22,31 @@ const fetchData = async () => {
     }
 };
 
+allPlanets.forEach(function (planet) {
+    planet.addEventListener("click", function () {
+        const clickedElementId = planet.id;
+        planetInfo(clickedElementId);
+        /* console.log(clickedElementId); */
+    })
+});
+
 const planetInfo = (planetId) => {
     const planet = planetData.find(item => item.name === planetId);
     if (planet) {
-        // Här kan du visa informationen om planeten, t.ex. i en modal eller någon annan del av din webbsida
+        // Här kan du visa informationen om planeten, t.ex. i en modal
         // Koppla overlay av/på här?
+       /*  const moonNames = planet.moons.map((element) => (element)); */
+        /* const moonElem =  */document.getElementById("moons").innerHTML = planet.moons.join(", ");
+        /* moonElem.innerHTML = moonNames.join(", "); */
         document.getElementById("overlayContainer").style.display = "block";
+        document.getElementById("name").innerHTML = planet.name;
+        document.getElementById("latinName").innerHTML = planet.latinName;
+        document.getElementById("desc").innerHTML = planet.desc;
+        document.getElementById("circumference").innerHTML = planet.circumference + " KM";
+        document.getElementById("distance").innerHTML = planet.distance + " KM";
+        document.getElementById("tempDay").innerHTML = planet.temp.day + " C";
+        document.getElementById("tempNight").innerHTML = planet.temp.night + " C";
+
 
         console.log(planet);
     } else {
@@ -35,22 +54,27 @@ const planetInfo = (planetId) => {
     }
 };
 
-allPlanets.forEach(function (planet) {
-    planet.addEventListener("click", function () {
-        const clickedElementId = planet.id;
-        planetInfo(clickedElementId);
-        /* console.log(clickedElementId); */
-    })
-})
-/* 
-function displayOn() {
-    document.getElementById("overlay").style.display = "block";
+
+
+function closeOverlay() {
+    document.getElementById("overlayContainer").style.display = "none";
 }
 
-function displayOff() {
-    document.addEventListener("click", function () {
-        document.getElementById("overlay").style.display = "none";
-    })
+// Skapa stjärnor funktionen
+const starContainer = document.querySelector(".stars");
+
+function createStars() {
+    for (let i = 0; i < 58; i++) {
+        const star = document.createElement("div");
+        star.className = "star";
+        star.style.left = `${Math.random() * 100}%`;
+        star.style.top = `${Math.random() * 100}%`;
+        starContainer.appendChild(star);
+    }
 }
- */
+
+createStars();
+
+
+
 fetchData(); 
